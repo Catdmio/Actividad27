cb1589f7b660221face1d8f78de9009f6c7df0db
Catdmio <montecinos.catalina@gmail.com>
2017-11-06 20:46:39 -0300

    added stuff on products controller such as new create destry methods

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		@product.new(product_params)
+		@product.category_id = params[:category_id]
+		@product.save
+		if @product.save
+			redirect_to category_path(@product.category_id)
+		else
+			redirect_to category_path(params[:category_id])
+		end
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to product_path
+	end
+
+	private
+	def product_params
+		params.require(:product).permit(:name, :price)
+	end
+end
